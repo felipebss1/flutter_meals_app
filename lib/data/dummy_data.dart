cec2069f8857ef90c811fd97a68a94e5219016b5
Bruno Soares <85403673+felipebss1@users.noreply.github.com>
2023-03-07 16:45:22 -0300

    Minor Changes

--- lib/data/dummy_data.dart
+++ lib/data/dummy_data.dart
@@ -1,3 +1,5 @@
+// ignore_for_file: constant_identifier_names
+
 import 'package:flutter/material.dart';
 import 'package:flutter_meals_app/models/category.dart';
 import 'package:flutter_meals_app/models/meal.dart';
--- lib/models/meal.dart
+++ lib/models/meal.dart
@@ -1,4 +1,4 @@
-// ignore_for_file: public_member_api_docs, sort_constructors_first
+// ignore_for_file: public_member_api_docs, sort_constructors_first, constant_identifier_names
 enum Complexity {
   Simple,
   Challenging,
